cfcaad369163f7f84a8cf1728bdb4a5738ce910a
Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>
2020-04-27 22:22:32 +0200

    add doxygen comments
    Signed-off-by: Mattéo Rossillol‑‑Laruelle <beatussum@protonmail.com>

--- src/dbus/DBusAbstractInterface.hpp
+++ src/dbus/DBusAbstractInterface.hpp
@@ -22,21 +22,56 @@
 #include <QtDBus/QDBusInterface>
 #include <QtDBus/QDBusReply>
 
+
+/**
+ * @class DBusException dbus/DBusAbstractInterface.hpp
+ * @brief An exception in case of DBus error
+ */
 class DBusException final : public std::runtime_error
 {
 public:
+    /**
+     * @brief Construct a new DBusException object
+     *
+     * @param e the QDBusError object
+     */
     DBusException(QDBusError e);
 
+    /**
+     * @brief Get some information about the exception
+     *
+     * @return The message
+     */
     const char* what() const noexcept override;
 private:
     QDBusError m_error_;
 };
 
+/**
+ * @class DBusAbstractInterface dbus/DBusAbstractInterface.hpp
+ * @brief A base class for all DBus interfaces
+ *
+ * This class provides some useful methods in order to avoid
+ * repetitive code.
+ */
 class DBusAbstractInterface : public QDBusInterface
 {
 public:
+    /**
+     * @brief The name of the DBus service
+     *
+     * This constant can have different values depending on
+     * whether **RATBAG_TEST** is set or not.
+     */
     static const QString m_kServiceName;
 public:
+    /**
+     * @brief Construct a new DBusAbstractInterface object
+     *
+     * @param interface the interface name without the root prefix: e.g.
+     * _button_ not _org.example.button_
+     * @param obj       the DBusAbstractInterface object
+     */
     DBusAbstractInterface(const QString& interface, const QString& obj) noexcept
         : m_interface_(m_kServiceName + '.' + interface)
         , m_obj_(obj)
@@ -44,8 +79,24 @@ public:
                          m_obj_, QDBusConnection::systemBus())
         {}
 
+    /**
+     * @brief An alias to QDBusConnection::connect()
+     *
+     * @param  name     the name of the signal
+     * @param  receiver the object receiving the signal
+     * @param  slot     the name of the slot
+     * @return true     if the connection has been successful
+     * @return false    if the connection has not been successful
+     */
     bool connect(const QString& name, QObject* receiver, const char* slot);
 
+    /**
+     * @brief Get the property value
+     *
+     * @tparam T        the type returned by the property
+     * @param  property the name of the property
+     * @return T        the type returned by the property
+     */
     template<class T>
     T getProperty(const QString& property);
 private:
